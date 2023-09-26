4cef15a26ce8f0cfa6f60a10f9e88207af0caf25
PetitHomardBreton <pierre-henri@barbe.contact>
2023-09-26 14:54:16 +0200

    premier commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function toggleMenu () {  
+    const navbar = document.querySelector('.navbar');
+    const burger = document.querySelector('.burger');
+    const logoLeft = document.querySelector('#logoLeft');
+    const headerNav = document.querySelector('header nav');
+    
+        
+    burger.addEventListener('click', (e) => {    
+      navbar.classList.toggle('show-nav');
+      logoLeft.classList.toggle('hide-logoLeft');
+      headerNav.classList.toggle('no-border'); 
+  });
+          
+
+    const navbarLinks = document.querySelectorAll('.navbar a');
+    navbarLinks.forEach(link => {
+      link.addEventListener('click', (e) => {    
+        navbar.classList.toggle('show-nav');
+        logoLeft.classList.toggle('hide-logoLeft');
+        headerNav.classList.toggle('no-border');
+      }); 
+    })
+     
+  }
+  toggleMenu();
