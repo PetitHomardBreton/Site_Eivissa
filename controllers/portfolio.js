50de48abe85fdcfea396844d109b98ea1458d981
PetitHomardBreton <pierre-henri@barbe.contact>
2023-10-13 21:05:32 +0200

    Read des réalisation dans la page Portfolio : OK

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -1,20 +1,30 @@
-/*export default (req, res) => {
-    res.render('portfolio', { pageTitle: 'Portfolio', fullPage: false });
-}*/
 import query from '../database.js'; // Assurez-vous que cet import est correct
 
 export default (req, res) => {
-    query('SELECT nameTheme FROM themes ORDER BY rankingTheme ASC', [], (error, results) => {
-        if (error) {
-            console.error(error);
-            res.status(500).send('Erreur lors de la requête');
+    // Première requête pour obtenir les thèmes
+    query('SELECT nameTheme FROM themes ORDER BY rankingTheme ASC', [], (errorThemes, resultsThemes) => {
+        if (errorThemes) {
+            console.error(errorThemes);
+            res.status(500).send('Erreur lors de la requête des thèmes');
             return;
         }
 
-        const themes = results.map(row => row.nameTheme);
-        res.render('portfolio', { pageTitle: 'Portfolio', fullPage: false, themes });
+        const themes = resultsThemes.map(row => row.nameTheme);
+
+        // Deuxième requête pour obtenir les réalisations
+        query('SELECT realisations.*, visuels.visuelWidth575 FROM realisations JOIN visuels ON realisations.idVisuelPrincipal = visuels.id ORDER BY rankingRealisation ASC', [], (errorRealisations, resultsRealisations) => {
+            if (errorRealisations) {
+                console.error(errorRealisations);
+                res.status(500).send('Erreur lors de la requête des réalisations');
+                return;
+            }
+
+            res.render('portfolio', {
+                pageTitle: 'Portfolio',
+                fullPage: false,
+                themes,
+                realisations: resultsRealisations
+            });
+        });
     });
 };
-
-
-
