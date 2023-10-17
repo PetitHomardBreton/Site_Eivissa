f7952b50ba2fd5a609f30967d83c9a3d03e1b947
PetitHomardBreton <pierre-henri@barbe.contact>
2023-10-17 17:28:26 +0200

    Fin d'aprem

--- controllers/readAllVisuelsByRealisationController.js
+++ controllers/readAllVisuelsByRealisationController.js
@@ -1,20 +1,19 @@
-import { getAllVisuelsByRealisationId } from '../models/visuelModel.js';
+import { getAllVisuelsByRealisationId } from "../models/visuelModel.js";
 
 export default (req, res) => {
-    const realisationId = req.params.realisationId;
-    getAllVisuelsByRealisationId(realisationId,(errorVisuels, visuels) => {
-        if (errorVisuels) {
-            console.error(errorVisuels);
-            res.status(500).send('Erreur lors de la requête des visuels');
-            return;
-        }
-
-        res.render('readAllVisuelsByRealisation', {
-            pageTitle: 'VisuelsByRealisation', 
-            visuels: visuels, 
-            actionDelete: "/realisations/" + realisationId + "/visuels/delete",
-            actionAdd: "/realisations/" + realisationId + "/visuels/add"
-        });
+  const realisationId = req.params.realisationId;
+  getAllVisuelsByRealisationId(realisationId, (errorVisuels, visuels) => {
+    if (errorVisuels) {
+      console.error(errorVisuels);
+      res.status(500).send("Erreur lors de la requête des visuels");
+      return;
+    }
+    res.render("readAllVisuelsByRealisation", {
+      pageTitle: "VisuelsByRealisation",
+      visuels: visuels,
+      nomRealisation: visuels[0].nomRealisation,
+      actionDelete: "/realisations/" + realisationId + "/visuels/delete",
+      actionAdd: "/realisations/" + realisationId + "/visuels/add",
     });
+  });
 };
-
